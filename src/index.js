eb7b2b9b312f2923e678c98e21954094fa8d5a61
Jhonatan Escalante <jhony.95.can@gmail.com>
2020-07-23 00:12:09 -0500

    Learning JSX

--- src/index.js
+++ src/index.js
@@ -8,9 +8,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = <h1>Hello, Platzi Badges!</h1>;
+// const element = <h1>Hello, Platzi Badges!</h1>;
+// const element = React.createElement('a', {href:'https://platzi.com'}, 'Ir a Platzi');
+// const name = 'Rodolfo';
+// const element = React.createElement(
+//     'h1',
+//     {},
+//     `Hola Soy ${name}`
+// );
+
+// const sum = (num1, num2) => num1 + num2;
+// const jsx = <h1>Hola soy, {undefined}</h1>
+
+const jsx = (<div>
+    <h1>
+        Hola, soy Jhonatan
+    </h1>
+    <p>Soy ingeniero de software</p>
+</div>);
 
 const container = document.getElementById('app');
 
 // ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(element, container);
+ReactDOM.render(jsx, container);
