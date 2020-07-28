9ef7ef095d1c0edc7c6dd063a4fca3b9db071e5e
Jhonatan Escalante <jhony.95.can@gmail.com>
2020-07-27 22:37:46 -0500

    Stating with a form

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -4,6 +4,7 @@ import './styles/BadgeNews.css';
 import header from '../images/badge-header.svg';
 import Navbar from '../components/Navbar';
 import Badge from '../components/Badge';
+import BadgeForm from '../components/BadgeForm';
 
 class BadgeNew extends React.Component {
     render() {
@@ -15,7 +16,7 @@ class BadgeNew extends React.Component {
                 </div>
                 <div className="container">
                     <div className="row">
-                        <div className="col">
+                        <div className="col-6">
                         <Badge
                             firstName="Jhonatan" 
                             lastName="Escalante" 
@@ -23,6 +24,9 @@ class BadgeNew extends React.Component {
                             jobTitle="Frontend Engineer" 
                             twitter="jhogakuse"
                         />
+                        </div>
+                        <div className="col-6">
+                            <BadgeForm/>
                         </div>
                     </div>
                 </div>
